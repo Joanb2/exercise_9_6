f065556e59fd9d9cee49e6af91f9cc9c893acef7
Joanna Buchała <joanna.b2@op.pl>
2018-04-06 23:37:42 +0200

    add content

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+var element = document.createElement('li');
+
+console.log(list.getElementsByTagName('li'));
+
+add.addEventListner('click', function() {});
+
+function add() {
+	element;
+	
+}
