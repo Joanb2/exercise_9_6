3912001648a7e51cba347f7a788ea51bbc098577
Joanna Buchała <joanna.b2@op.pl>
2018-04-07 18:46:34 +0200

    add content2

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 var list = document.getElementById('list');
 var add = document.getElementById('addElem');
-var element = document.createElement('li');
+var element = document.createElement('click','li')[0];
+var length = list.getElementsByTagName('li');
 
-console.log(list.getElementsByTagName('li'));
+addElem.addEventListner('click', create(addElem));
 
-add.addEventListner('click', function() {});
-
-function add() {
+function create() {
 	element;
-	
+	element.innerHTML = 'item';
+	list.appendChild(element);
 }
