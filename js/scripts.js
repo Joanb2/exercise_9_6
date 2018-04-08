4460fd80ad0723c4d85e92ded2aabd24eac9239c
Joanna Buchała <joanna.b2@op.pl>
2018-04-08 13:31:51 +0200

    change content

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 var list = document.getElementById('list');
 var add = document.getElementById('addElem');
-var element = document.createElement('click','li')[0];
-var length = list.getElementsByTagName('li');
 
-addElem.addEventListner('click', create(addElem));
-
-function create() {
-	element;
-	element.innerHTML = 'item';
+add.addEventListener('click', function()
+{
+	var element = document.createElement('li');
+	var length = document.getElementsByTagName('li').length;
+	element.innerHTML = 'item ' + length;
 	list.appendChild(element);
-}
+});
